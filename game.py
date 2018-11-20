8104cb3874e3d9fa5a0fed7bb629126e096f5dcd
SHEKK0 <jadomen@inf.upv.es>
2018-11-20 12:05:11 +0100

    terminado?

--- game.py
+++ game.py
@@ -15,6 +15,11 @@ ADDNEWOBJECTRATE = 10
 MAX_SIZE = 40
 MIN_SIZE = 10
 
+OBJECT_MAX_SPEED = 7
+OBJECT_MIN_SPEED = 1
+PLAYERMOVERATE = 5
+
+
 def terminate():
     pygame.quit()
     sys.exit()
@@ -103,8 +108,50 @@ while True:
         if objectAddCounter == ADDNEWOBJECTRATE
             objectAddCounter = 0
             objectSize = random.randint(MAX_SIZE, MIN_SIZE)
-            newObject = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-baddieSize), 0 - baddieSize, baddieSize, baddieSize),
-                         'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
+            newObject = {'rect': pygame.Rect(random.randint(0, WINDOW_WIDTH-objectSize), 0 - objectSize, objectSize, objectSize),
+                         'speed': random.randint(OBJECT_MIN_SPEED, OBJECT_MAX_SPEED),
                          'surface':pygame.transform.scale(baddieImage, (baddieSize, baddieSize)),
                          }
             objects.append(newObject)
+
+        if moveRight and playerRect.right < WINDOW_WIDTH:
+            playerRect.move_ip(PLAYERMOVERATE, 0)
+        if moveLeft and playerRect.left > 0:
+            playerRect.move_ip(-1* PLAYERMOVERATE)
+        if moveUP and playerRect.top < 0:
+            playerRect.move_ip(0,PLAYERMOVERATE)
+        if moveDown and playerRect.botton < WINDOW_WEIGHT:
+            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
+
+        pygame.mouse.set_pos(playerRect.centerx, playerRect.centery)
+
+        for b in objects:
+                if not reverseCheat and not slowCheat:
+                    b['rect'].move_ip(0,b['speed'])
+                else reverseCheat:
+                    b['rect'].move_ip(0,5)
+                else slowCheat:
+                    b['rect'].move_ip(0,1)
+#DELETE OBJECTS THAT HAD FALL OVER THE LIMITS
+        for b in objects[:]:
+            if b['rect'].top > WINDOW_WEIGHT:
+                objects.remove(b)
+
+        windowSurface.fill(BACKGROUND_COLOUR)
+
+        for b in objects:
+            windowSurface.blit(b['surface'], b['rect'])
+
+        pygame.display.update()
+
+        if playerHasHitObject(playerRect,objects):
+            if score > topScore
+                topScore = score
+            break
+
+        mainClock.tick(FPS)
+
+        drawText('Game Over putos',font, windowSurface,(WINDOW_WIDTH/2), (WINDOW_WEIGHT/2))
+        drawText('Pulsa algo pa intentar otra vez puto malo',font, windowSurface,(WINDOW_WIDTH/2)-50,(WINDOW_WEIGHT/2))
+        pygame.display.update()
+        waitForPlayerToPressKey()
